b47b367c3f4f071ed4f14c601d351e5c59e3d97d
Rob Gilbert <rgilbert90@gmail.com>
2019-03-01 17:02:15 -0800

    finalizing JS scripts

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Hello!");
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Hello " + firstName + " " + lastName);
+console.log("You are " + age + " years old.");
